84ab739a79ebcf08b653bc5d277141c2b970b316
Dylberto <dylang565@gmail.com>
2018-11-14 19:02:18 +0000

    TestV2

--- Baslings/Assets/Scripts/PlayerController.cs
+++ Baslings/Assets/Scripts/PlayerController.cs
@@ -2,39 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public int player_speed;
+    public float jump_force;
+    public float player_speed;
     private CharacterController _controller;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         _controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //Movement
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _controller.SimpleMove(move * player_speed);
 
-
-
-
-        //Changing color
-
-        if (Input.GetKeyDown(KeyCode.R))
+        //Jump
+        if (Input.GetButtonDown("Jump"))
         {
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            if (_controller.isGrounded)
+            {
+                move.y = jump_force;
+            }
+
+            //Changing color
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+            }
 
         }
-
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            gameObject.GetComponent<Renderer>().material.color = Color.blue;
-
-        }
-
     }
 }
